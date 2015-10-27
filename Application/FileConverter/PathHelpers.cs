e0d290306b5a2c5b2f5652a2e10f91fb70c273ca
Tichau <tsi.allard@gmail.com>
2015-10-27 19:23:00 +0100

    Added: Create folders when necessary.

--- Application/FileConverter/PathHelpers.cs
+++ Application/FileConverter/PathHelpers.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿// <copyright file="PathHelpers.cs" company="AAllard">License: http://www.gnu.org/licenses/gpl.html GPL version 3.</copyright>
 
 namespace FileConverter
 {
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
     public static class PathHelpers
     {
         private static Regex driveLetterRegex = new Regex(@"[a-zA-Z]:\\");
         private static Regex pathRegex = new Regex(@"^[a-zA-Z]:\\(?:[^\\/:*?""<>|\r\n]+\\)*[^\.\\/:*?""<>|\r\n][^\\/:*?""<>|\r\n]*$");
         private static Regex filenameRegex = new Regex(@"[^\\]*", RegexOptions.RightToLeft);
+        private static Regex directoryRegex = new Regex(@"(?:([^\\]*)\\)*");
 
         public static bool IsPathDriveLetterValid(string path)
         {
@@ -28,6 +27,22 @@ namespace FileConverter
             MatchCollection matchCollection = PathHelpers.filenameRegex.Matches(path);
             Match filenameMatch = matchCollection.Count > 0 ? matchCollection[0] : null;
             return filenameMatch?.Groups[0].Value;
+        }
+        
+        public static IEnumerable<string> GetDirectories(string path)
+        {
+            MatchCollection matchCollection = PathHelpers.directoryRegex.Matches(path);
+            Match match = matchCollection.Count > 0 ? matchCollection[0] : null;
+            if (match == null || match.Groups.Count < 2)
+            {
+                yield break;
+            }
+
+            Group matchGroup = match.Groups[1];
+            for (int index = 0; index < matchGroup.Captures.Count; index++)
+            {
+                yield return matchGroup.Captures[index].Value;
+            }
         }
     }
 }
