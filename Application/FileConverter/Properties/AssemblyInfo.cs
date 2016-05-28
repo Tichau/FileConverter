064aad972ef52b95e7b8bbcd98ff775e4a3c6eb4
Tichau <tsi.allard@gmail.com>
2016-05-28 17:34:36 +0200

    Update project infos

--- Application/FileConverter/Properties/AssemblyInfo.cs
+++ Application/FileConverter/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FileConverter")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
