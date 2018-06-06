ca6e2c89198cea110cb9a7f2b89b64dfc40f45ec
Adrien Allard <adrien.allard.pro@gmail.com>
2018-06-06 08:35:46 +0200

    Add conversion jobs to progress indicators converters

---
+++ Application/FileConverter/ValueConverters/ConversionJobsToProgressState.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="ConversionJobsToProgressState.cs" company="AAllard">License: http://www.gnu.org/licenses/gpl.html GPL version 3.</copyright>
+
+namespace FileConverter.ValueConverters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Shell;
+
+    using FileConverter.ConversionJobs;
+
+    public class ConversionJobsToProgressState : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is IEnumerable<ConversionJob>))
+            {
+                return TaskbarItemProgressState.None;
+            }
+
+            IEnumerable<ConversionJob> jobs = (IEnumerable<ConversionJob>)value;
+
+            TaskbarItemProgressState progressState = TaskbarItemProgressState.None;
+            foreach (ConversionJob job in jobs)
+            {
+                if (job.State == ConversionJob.ConversionState.Failed)
+                {
+                    progressState = TaskbarItemProgressState.Error;
+                    break;
+                }
+
+                if (job.State == ConversionJob.ConversionState.InProgress)
+                {
+                    progressState = TaskbarItemProgressState.Normal;
+                    break;
+                }
+            }
+
+            return progressState;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/FileConverter/ValueConverters/ConversionJobsToProgressValue.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="ConversionJobsToProgressValue.cs" company="AAllard">License: http://www.gnu.org/licenses/gpl.html GPL version 3.</copyright>
+
+namespace FileConverter.ValueConverters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Shell;
+
+    using FileConverter.ConversionJobs;
+
+    public class ConversionJobsToProgressValue : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is IEnumerable<ConversionJob>))
+            {
+                return TaskbarItemProgressState.None;
+            }
+
+            IEnumerable<ConversionJob> jobs = (IEnumerable<ConversionJob>)value;
+
+            int jobCount = 0;
+            float progressValue = 0f;
+            foreach (ConversionJob job in jobs)
+            {
+                progressValue += job.Progress;
+                jobCount++;
+            }
+
+            return jobCount > 0 ? progressValue / jobCount : 0f;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
