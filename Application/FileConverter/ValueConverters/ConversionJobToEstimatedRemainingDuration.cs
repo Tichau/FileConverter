7daf387f8adfd6ab75551d9867d8d19d08923aab
Adrien Allard <adrien.allard.pro@gmail.com>
2018-06-19 19:20:07 +0200

    Add ConversionJobToEstimatedRemainingDuration

---
+++ Application/FileConverter/ValueConverters/ConversionJobToEstimatedRemainingDuration.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="ConversionJobsToProgressValue.cs" company="AAllard">License: http://www.gnu.org/licenses/gpl.html GPL version 3.</copyright>
+
+namespace FileConverter.ValueConverters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    using ConversionJobs;
+
+    public class ConversionJobToEstimatedRemainingDuration : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 3)
+            {
+                return string.Empty;
+            }
+
+            ConversionJob.ConversionState state = (ConversionJob.ConversionState)values[0];
+            DateTime startTime = (DateTime)values[1];
+            float progress = (float)values[2];
+
+            if (state == ConversionJob.ConversionState.Unknown ||
+                state == ConversionJob.ConversionState.Ready ||
+                progress < 0.05f)
+            {
+                return "-";
+            }
+
+            if (progress >= 1f)
+            {
+                return "-";
+            }
+
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+
+            double remainingTimeInSeconds = (1 - progress) * elapsedTime.TotalSeconds / progress;
+            TimeSpan remainingTime = TimeSpan.FromSeconds(Math.Floor(remainingTimeInSeconds));
+            return remainingTime.ToString("g");
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
