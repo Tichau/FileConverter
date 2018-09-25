2f7a00c10b7d3e327dfb23651948d523b7c448f4
Adrien Allard <adrien.allard.pro@gmail.com>
2018-09-25 18:48:50 +0200

    Improve display of remaining time

--- Application/FileConverter/ValueConverters/ConversionJobToEstimatedRemainingDuration.cs
+++ Application/FileConverter/ValueConverters/ConversionJobToEstimatedRemainingDuration.cs
@@ -23,21 +23,21 @@ namespace FileConverter.ValueConverters
 
             if (state == ConversionState.Unknown ||
                 state == ConversionState.Ready ||
-                progress < 0.05f)
+                state == ConversionState.Failed ||
+                state == ConversionState.Done)
             {
-                return "-";
-            }
-
-            if (progress >= 1f)
-            {
-                return "-";
+                return string.Empty;
             }
 
             TimeSpan elapsedTime = DateTime.Now - startTime;
+            if (elapsedTime.TotalSeconds < 10f)
+            {
+                return string.Empty;
+            }
 
             double remainingTimeInSeconds = (1 - progress) * elapsedTime.TotalSeconds / progress;
             TimeSpan remainingTime = TimeSpan.FromSeconds(Math.Floor(remainingTimeInSeconds));
-            return remainingTime.ToString("g");
+            return "~" + remainingTime.ToString("g");
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
