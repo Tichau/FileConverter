c7605c84010c4083b369cda5cbcc969a056b0fcc
Adrien Allard <adrien.allard.pro@gmail.com>
2020-11-13 15:04:34 +0100

    Fixed: Typo in localization keys

--- Application/FileConverter/Properties/Resources.Designer.cs
+++ Application/FileConverter/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace FileConverter.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1060,9 +1060,9 @@ namespace FileConverter.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Recommended bitrate range in blue.
         /// </summary>
-        public static string RecommendeBitrateRangeInBlue {
+        public static string RecommendedBitrateRangeInBlue {
             get {
-                return ResourceManager.GetString("RecommendeBitrateRangeInBlue", resourceCulture);
+                return ResourceManager.GetString("RecommendedBitrateRangeInBlue", resourceCulture);
             }
         }
         
