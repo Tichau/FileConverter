1154e7de9fbf5940c3037063943c2851a254e484
Adrien Allard <adrien.allard.pro@gmail.com>
2020-11-16 21:50:59 +0100

    Move conversion state colors in resource files

--- Application/FileConverter/ValueConverters/ConversionStateToBrush.cs
+++ Application/FileConverter/ValueConverters/ConversionStateToBrush.cs
@@ -21,52 +21,7 @@ namespace FileConverter.ValueConverters
             ConversionState conversionState = (ConversionState)value;
             string type = parameter as string;
 
-            if (type == "Background")
-            {
-                switch (conversionState)
-                {
-                    case ConversionState.Unknown:
-                        return new SolidColorBrush(Color.FromRgb(230, 230, 230));
-
-                    case ConversionState.Ready:
-                        return new SolidColorBrush(Color.FromRgb(230, 230, 230));
-
-                    case ConversionState.InProgress:
-                        return new SolidColorBrush(Color.FromRgb(250, 236, 179));
-
-                    case ConversionState.Done:
-                        return new SolidColorBrush(Color.FromRgb(200, 230, 201));
-
-                    case ConversionState.Failed:
-                        return new SolidColorBrush(Color.FromRgb(255, 205, 210));
-                }
-            }
-            else if (type == "Foreground")
-            {
-                switch (conversionState)
-                {
-                    case ConversionState.Unknown:
-                        return new SolidColorBrush(Color.FromRgb(128, 128, 128));
-
-                    case ConversionState.Ready:
-                        return new SolidColorBrush(Color.FromRgb(32, 32, 32));
-
-                    case ConversionState.InProgress:
-                        return new SolidColorBrush(Color.FromRgb(255, 160, 0));
-
-                    case ConversionState.Done:
-                        return new SolidColorBrush(Color.FromRgb(56, 142, 60));
-
-                    case ConversionState.Failed:
-                        return new SolidColorBrush(Color.FromRgb(255, 82, 82));
-                }
-            }
-            else
-            {
-                throw new SystemException("Unknown type.");
-            }
-            
-            throw new SystemException("Unknown state.");
+            return Application.Current.Resources[$"{conversionState}{type}Brush"];
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
