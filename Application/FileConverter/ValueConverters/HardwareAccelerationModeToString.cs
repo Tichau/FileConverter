5f7bcadb83c94bd188b4dad7c7c9d7422651da74
tacheometry <39647014+tacheometry@users.noreply.github.com>
2024-03-26 16:10:01 +0200

    Add setting for hardware acceleration mode

---
+++ Application/FileConverter/ValueConverters/HardwareAccelerationModeToString.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="HardwareAccelerationModeToString.cs" company="AAllard">License: http://www.gnu.org/licenses/gpl.html GPL version 3.</copyright>
+
+namespace FileConverter.ValueConverters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class HardwareAccelerationModeToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value) {
+                case Helpers.HardwareAccelerationMode.Off:
+                    return "Off";
+                case Helpers.HardwareAccelerationMode.CUDA:
+                    return "Nvidia (CUDA)";
+            }
+            return "Unknown";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
